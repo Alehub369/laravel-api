6d228d1efd08c04d181906b9c45f645da74e5bc5
Alehub369 <lievanoalejandro@gmail.com>
2023-11-24 14:36:53 -0500

    Consulta de datos (Rutas y controladores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,34 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(29)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            \App\Models\User::factory()->create([
+                'name' => 'Ale',
+                'email' => 'ale123456789@app.com',
+            ]);
+
+        Category::factory(12)->create();
+        Recipe::factory(100)->create();
+        Tag::factory(40)->create();
+
+        // Many to many
+        $recipes = Recipe::all();
+        $tags = Tag::all();
+        
+        foreach($recipes as $recipe) {
+            $recipe->tags()->attach($tags->random(rand(2, 4)));
+        }
     }
 }
