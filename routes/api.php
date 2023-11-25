db4881fe83fe7212ea6d432f46f52e93cdabce57
Alehub369 <lievanoalejandro@gmail.com>
2023-11-24 20:16:30 -0500

    ¿Cómo planificar el alcance de tus APIs?

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('categories',             [CategoryController::class, 'index']);
 Route::get('categories/{category}',  [CategoryController::class, 'show']);
 
-Route::get('recipes',                [RecipeController::class, 'index']);
-Route::get('recipes/{recipe}',       [RecipeController::class, 'show']);
+// Codigo largo
 
-Route::get('tags',                    [TagController::class, 'index']);
+/* 
+
+Route::get('recipes',                [RecipeController::class, 'index']);
+Route::post('recipes',                [RecipeController::class, 'store']);
+Route::get('recipes/{recipe}',       [RecipeController::class, 'show']);
+Route::put('recipes/{recipe}',       [RecipeController::class, 'update']);
+Route::delete('recipes/{recipe}',       [RecipeController::class, 'destroy']);
+ */
+
+// Codigo corto mismo resultado
+
+Route::apiResource('recipes', RecipeController::class);
+
+Route::get('tags',                   [TagController::class, 'index']);
 Route::get('tags/{tag}',             [TagController::class, 'show']);
